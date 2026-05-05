dbaad4256cff24717edab3768b3cf322d7e0a1b6
NikolaTomic00 <tomicnikolaftn@gmail.com>
2026-05-05 18:37:52 +0200

    Updated HomePage, only text 'loading notes' is displaying while fetching data

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -42,11 +42,11 @@ const HomePage = () => {
       {isRateLimited && <RateLimitedUI />}
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
-        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
-
-        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
-
-        {notes.length > 0 && !isRateLimited && (
+        {loading ? (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        ) : notes.length === 0 && !isRateLimited ? (
+          <NotesNotFound />
+        ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
               <NoteCard key={note._id} note={note} setNotes={setNotes} />
